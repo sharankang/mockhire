76e0b4d122b62a611a8c97b2cc447abda3f6e839
sharankang <sharanpreetkaur2401@gmail.com>
2026-02-27 03:44:54 +0530

    frontend fix

--- js/resume.js
+++ js/resume.js
@@ -1,16 +1,7 @@
-if (localStorage.getItem("isLoggedIn") !== "true") {
+if (!localStorage.getItem("mockhireToken")) {
   alert("You must log in first.");
   window.location.href = "login.html";
 }
 
-const activeUser = localStorage.getItem("activeUser");
-let users = JSON.parse(localStorage.getItem("mockhireUsers") || "{}");
-
-if (!activeUser || !users[activeUser]) {
-  alert("Invalid session. Please log in again.");
-  localStorage.removeItem("isLoggedIn");
-  localStorage.removeItem("activeUser");
-  window.location.href = "login.html";
-}
-
+const activeUser = localStorage.getItem("mockhireUser");
 console.log("Resume templates page loaded for:", activeUser);
